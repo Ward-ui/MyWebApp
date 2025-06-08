6851f7f95bcd7f51b3f9d031badaaf27355e72f5
Ward-ui <gagagagagagag1189@gmail.com>
2025-06-08 16:30:06 +0400

    Удалил проверку токена на главной и на продутках

--- server/models/index.js
+++ server/models/index.js
@@ -5,8 +5,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 // Подключение к базе данных
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: 'mysql',
+    port: process.env.DB_PORT,
     logging: false
 });
 
